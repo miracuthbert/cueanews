e1c6a604a0305f31035652acca50148b0c0665d0
miracuthbert <miracuthbert@gmail.com>
2017-10-12 23:49:09 +0300

    Update app config
    Updated app name
    Registered Barryvdh dompdf wrapper facade

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'CUEA NEWS'),
+    'name' => 'CUEA411',
 
     /*
     |--------------------------------------------------------------------------
@@ -231,7 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
