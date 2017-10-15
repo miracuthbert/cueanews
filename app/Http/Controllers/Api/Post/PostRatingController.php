ba9dd2e97884c620ce5661604e0e41de18d62324
miracuthbert <miracuthbert@gmail.com>
2017-10-15 20:17:41 +0300

    Add Api PostRatingController
    Added functionality for user's post rating  in store and destroy method

---
+++ app/Http/Controllers/Api/Post/PostRatingController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Api\Post;
+
+use App\Models\Post;
+use App\Models\Rate;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostRatingController extends Controller
+{
+    /**
+     * PostRatingController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Post $post)
+    {
+        $user = $request->user();
+
+        if ($user->hasRatedPost($post)) {
+            return response(null, 409);
+        }
+
+        $rate = new Rate();
+        $rate->user()->associate($user);
+        $rate->rating = $request->rating;
+        $post->ratings()->save($rate);
+
+        return response(null, 204);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Request $request
+     * @param  \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Post $post)
+    {
+        $user = $request->user();
+
+        if ($user->hasRatedPost($post)) {
+            $post->ratings->where('user_id', $user->id)->delete();
+
+            return response(null, 204);
+        }
+
+        return response(null, 409);
+    }
+}
