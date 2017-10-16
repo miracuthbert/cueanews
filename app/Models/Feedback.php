c522e3a498ea3d6f867a5a9d99b3576c70e0cb3f
miracuthbert <miracuthbert@gmail.com>
2017-10-16 13:37:46 +0300

    Update Feedback Model
    Fixed missing User model class import

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\Eloquent\OrderableTrait;
 use App\Traits\Eloquent\PivotOrderableTrait;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
